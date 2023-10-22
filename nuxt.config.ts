9438d8de40ce3541bccb881ef197825e62807f4b
ithiel883 <ajuka.zephiniah@gmail.com>
2023-10-22 20:10:37 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "SeyiCommerce | Your No 1 Ecommerce Site",
+      htmlAttrs: { lang: "en" },
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "SeyiCommerce is an ecommerce site that helps with procuring your favorite items at discount prices.",
+        },
+        {
+          hid: "viewport",
+          name: "viewport",
+          content: "width=device-width,initial-scale=1.0",
+        },
+      ],
+    },
+  },
+  css: ["~/assets/css/main.css"],
+  srcDir: "src/",
+  alias: {
+    "class-validator": "class-validator/cjs/index.js",
+  },
+  modules: ["@nuxt/ui"],
+
+  pages: true,
+  
+  devtools: {
+    // Enable devtools (default: true)
+    enabled: true,
+    // VS Code Server options
+    vscode: {},
+    // ...other options
+  },
+});
